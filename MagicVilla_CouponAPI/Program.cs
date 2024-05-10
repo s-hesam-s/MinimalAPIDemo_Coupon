59fe97a2f8eb613860f98727b518becd487c31fb
s-hesam-s <hesamshoushtarian@gmail.com>
2024-05-10 18:51:19 +0330

    Section 3 - Create Coupon

--- MagicVilla_CouponAPI/Program.cs
+++ MagicVilla_CouponAPI/Program.cs
@@ -1,4 +1,6 @@
 using MagicVilla_CouponAPI.Data;
+using MagicVilla_CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +26,18 @@ app.MapGet("/api/coupon/{id:int}", (int id) => {
     return Results.Ok(CouponStore.couponList.FirstOrDefault(u => u.Id == id));
 });
 
-app.MapPost("/api/coupon", () => {
+app.MapPost("/api/coupon", ([FromBody] Coupon coupon) => {
+    if (coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+    {
+        return Results.BadRequest("Invalid Id or Coupon Name");
+    }
+    if (CouponStore.couponList.FirstOrDefault(u => u.Name.ToLower() == coupon.Name.ToLower()) != null)
+    {
+        return Results.BadRequest("Coupon Name already Exists");
+    }
+    coupon.Id = CouponStore.couponList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+    CouponStore.couponList.Add(coupon);
+    return Results.Ok(coupon);
 });
 
 app.MapPut("/api/coupon", () => {
