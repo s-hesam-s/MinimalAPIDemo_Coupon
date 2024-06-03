4b87805b0455e4fd6dbcc6248068cfa7a40b5bb0
s-hesam-s <hesamshoushtarian@gmail.com>
2024-06-03 14:14:00 +0330

    Section 7 - Modify Auth Repository

--- MagicVilla_CouponAPI/Program.cs
+++ MagicVilla_CouponAPI/Program.cs
@@ -11,12 +11,18 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using MagicVilla_CouponAPI.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddSwaggerGen(option =>
 {
     option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
--- MagicVilla_CouponAPI/Repository/AuthRepository.cs
+++ MagicVilla_CouponAPI/Repository/AuthRepository.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 
 namespace MagicVilla_CouponAPI.Repository
 {
@@ -15,18 +16,26 @@ namespace MagicVilla_CouponAPI.Repository
         private readonly ApplicationDbContext _db;
         private readonly IMapper _mapper;
         private readonly IConfiguration _configuration;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private string secretKey;
-        public AuthRepository(ApplicationDbContext db, IMapper mapper, IConfiguration configuration)
+        public AuthRepository(ApplicationDbContext db,
+            IMapper mapper,
+            IConfiguration configuration,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
             _db = db;
             _mapper = mapper;
             _configuration = configuration;
+            _userManager = userManager;
+            _roleManager = roleManager;
             secretKey = _configuration.GetValue<string>("ApiSettings:Secret");
         }
 
         public bool IsUniqueUser(string username)
         {
-            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            var user = _db.ApplicationUsers.FirstOrDefault(x => x.UserName == username);
 
             // return null if user not found
             if (user == null)
@@ -37,14 +46,17 @@ namespace MagicVilla_CouponAPI.Repository
 
         public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            var user = _db.LocalUsers.SingleOrDefault(x => x.UserName == loginRequestDTO.UserName
-                                    && x.Password == loginRequestDTO.Password);
+            var user = _db.ApplicationUsers.SingleOrDefault(x => x.UserName == loginRequestDTO.UserName);
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDTO.Password);
 
             //user not found
-            if (user == null)
+            if (user == null || isValid == false)
             {
                 return null;
             }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(secretKey);
             var tokenDescriptor = new SecurityTokenDescriptor
@@ -52,7 +64,7 @@ namespace MagicVilla_CouponAPI.Repository
                 Subject = new ClaimsIdentity(new Claim[]
                 {
                     new Claim(ClaimTypes.Name,user.UserName),
-                    new Claim(ClaimTypes.Role,user.Role),
+                    new Claim(ClaimTypes.Role,roles.FirstOrDefault()),
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
@@ -69,17 +81,34 @@ namespace MagicVilla_CouponAPI.Repository
 
         public async Task<UserDTO> Register(RegisterationRequestDTO requestDTO)
         {
-            LocalUser userObj = new()
+            ApplicationUser userObj = new()
             {
                 UserName = requestDTO.UserName,
-                Password = requestDTO.Password,
                 Name = requestDTO.Name,
-                Role = "customer"
+                NormalizedEmail = requestDTO.UserName.ToUpper(),
+                Email = requestDTO.UserName,
             };
-            _db.LocalUsers.Add(userObj);
-            _db.SaveChanges();
-            userObj.Password = "";
-            return _mapper.Map<UserDTO>(userObj);
+            try
+            {
+                var result = await _userManager.CreateAsync(userObj, requestDTO.Password);
+                if (result.Succeeded)
+                {
+                    if (!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult())
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("admin"));
+                        await _roleManager.CreateAsync(new IdentityRole("customer"));
+                    }
+                    await _userManager.AddToRoleAsync(userObj, "admin");
+
+                    var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName == requestDTO.UserName);
+                    return _mapper.Map<UserDTO>(user);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
         }
     }
 }
